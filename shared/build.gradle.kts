5f62b3c276dcf2d2baf888ff4e9912506f46d1b6
Ernest Martin Gayyed <ernestgayyed@gmail.com>
2021-09-24 09:09:31 +0800

    add missing files

---
+++ shared/build.gradle.kts
@@ -0,0 +1,101 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    kotlin("plugin.serialization")
+    id("com.android.library")
+    id("com.squareup.sqldelight")
+}
+
+group = "jp.co.rakuten.oneapp"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+}
+
+kotlin {
+    android()
+    js(LEGACY) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    iosX64 {
+        binaries {
+            framework("discoveryrepo") {
+                baseName = "shared"
+                linkerOpts.add("-lsqlite3")
+            }
+        }
+    }
+
+    val coroutinesVersion = "1.5.0-native-mt"
+    val serializationVersion = "1.2.2"
+    val ktorVersion = "1.6.1"
+    val sqlDelightVersion: String by project
+
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.2.1")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13")
+            }
+        }
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation("com.squareup.sqldelight:sqljs-driver:1.5.0")
+                implementation("io.ktor:ktor-client-js:1.6.1")
+            }
+        }
+        val jsTest by getting
+        val iosX64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+                implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
+            }
+        }
+        val iosX64Test by getting
+    }
+}
+
+android {
+    compileSdkVersion(31)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(31)
+    }
+}
+
+sqldelight {
+    database("DiscoveryDatabase") {
+        packageName = "jp.co.rakuten.oneapp.shared.local"
+    }
+}
